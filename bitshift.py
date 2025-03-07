f6e42860d6d751e6bcd649d45c8c0349a7f5bc71
frostghost555 <83512260+frostghost555@users.noreply.github.com>
2025-03-07 13:44:46 -0800

    Update bitshift.py

--- bitshift.py
+++ bitshift.py
@@ -1,5 +1,8 @@
-def multiply(n):
-    #Add the code and an explanation on how to 
-    #multiply a number by 2 using bitshift
-    return n
-
+def multiply(a, b):
+  n=0
+  if a%n == 0:
+    a << 2**n
+  if b%n == 0:
+    b << 2**n
+    
+multiply(3,4)
