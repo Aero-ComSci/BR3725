315e7e23166433580ce77bb0ae68736342fedf60
Avinash Amanchi <148393523+avinashamanchi@users.noreply.github.com>
2025-03-07 13:35:34 -0800

    Update bitshift.py
    avinash amanchis repo

--- bitshift.py
+++ bitshift.py
@@ -1,5 +1,11 @@
-def multiply(n):
-    #Add the code and an explanation on how to 
-    #multiply a number by 2 using bitshift
+def multiply(a, b):
+    n = 0
+    while b > 0:
+        if b & 1:
+            n = n + a
+        a = a << 1
+        b = b >> 1
     return n
 
+n = multiply(6, 9)
+print(n)  
