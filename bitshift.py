8f9a6614f527ce6aee514f7909fa9a9fa80ce04c
bhavyad09 <157430117+bhavyad09@users.noreply.github.com>
2025-03-07 13:38:40 -0800

    Update bitshift.py

--- bitshift.py
+++ bitshift.py
@@ -1,5 +1,5 @@
-def multiply(n):
-    #Add the code and an explanation on how to 
-    #multiply a number by 2 using bitshift
-    return n
+def multiply(a, b):
+    return a << b
+
+multiply(5, 3)
 
