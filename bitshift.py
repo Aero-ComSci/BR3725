8f6c511743a1ef5d4ead81c60e85fbd7a1c12af0
sanjugowda216 <143921320+sanjugowda216@users.noreply.github.com>
2025-03-07 15:05:32 -0800

    message

--- bitshift.py
+++ bitshift.py
@@ -1,6 +1,8 @@
 def multiply(a,b):
     #Add the code and an explanation on how to 
     #multiply a number by 2 using bitshift
+    #doubling just like binary pattern is the only way to shift forward or back, and it is important to add what is left over especally when there is a product of an odd number
+    # this requires addition and an aproximation
     result=0
     while b>0:
         if (b&1) !=0:
