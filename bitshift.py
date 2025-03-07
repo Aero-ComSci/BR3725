299f1fa518d88dc4057e1c7e8249445899c4f29d
Vijayesh314 <145930677+Vijayesh314@users.noreply.github.com>
2025-03-07 15:09:03 -0800

    Update bitshift.py

--- bitshift.py
+++ bitshift.py
@@ -1,12 +1,12 @@
 def multiply(a, b):
     result = 0
-    while b > 0:
-        if b & 1:
-            result += a 
-        a <<= 1
-        b >>= 1
+    while b > 0: #While loop runs while there are bits, b is the multiplier
+        if b & 1: #Checks for least significant bit
+            result += a #Adds value for multiplication
+        a <<= 1 #Multiplies a by 2, sihfts all bits by 1 to the left
+        b >>= 1 #Divides b by 2, shifts all bits by 1 to the right
     return result
-
+    #Loop ends when b becomes 0
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 print(multiply(a, b))
