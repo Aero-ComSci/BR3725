b92655142623fab3c6e12d7bbe66612dd0fdd447
vivi-cake <157427005+vivi-cake@users.noreply.github.com>
2025-03-07 13:49:04 -0800

    Update bitshift.py

--- bitshift.py
+++ bitshift.py
@@ -1,6 +1,6 @@
 def multi():
      n1 = int(input("What is your intial number?"))
-     n2 = int(input('How much do you wnat to shift it by?'))
+     n2 = int(input('How much do you what power of two do you want to shift it by?'))
      n3 = n1 << n2
      print(n3)
 
