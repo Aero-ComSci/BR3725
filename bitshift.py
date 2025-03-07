acee05d9decbca0bf0c641d70281ce3f8ad7fc51
DC1309 <157426709+DylanCEHS@users.noreply.github.com>
2025-03-07 13:35:30 -0800

    Update bitshift.py

--- bitshift.py
+++ bitshift.py
@@ -1,5 +1,7 @@
 def multiply(n):
-    #Add the code and an explanation on how to 
-    #multiply a number by 2 using bitshift
-    return n
+create list with 5 digits
+when inputted how many shifts
+append the list in order to move each digit left 1
+assign each number a binary number, 1,2,4,8,16
+return n
 
