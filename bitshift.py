e1013a18d0009496856639afc0a6cf5d1c572e6f
vivi-cake <157427005+vivi-cake@users.noreply.github.com>
2025-03-07 13:41:47 -0800

    Update bitshift.py

--- bitshift.py
+++ bitshift.py
@@ -1,5 +1,10 @@
-def multiply(n):
-    #Add the code and an explanation on how to 
-    #multiply a number by 2 using bitshift
-    return n
+def multi():
+     n1 = int(input("What is your intial number?"))
+     n2 = int(input('How much do you wnat to shift it by?'))
+     n3 = n1 << n2
+     print(n3)
+
+multi()
+
+
 
