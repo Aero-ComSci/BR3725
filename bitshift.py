488fa26df9200b98f949283000c19e2480ef1c09
Aarav Goel <73917648+aaravgoel0@users.noreply.github.com>
2025-03-07 13:42:37 -0800

    Update bitshift.py

--- bitshift.py
+++ bitshift.py
@@ -1,5 +1,13 @@
 def multiply(n):
-    #Add the code and an explanation on how to 
-    #multiply a number by 2 using bitshift
-    return n
+    result = 0
+    while n > 0:
+        if n & 1:
+            result += n
+        n <<= 1
+        
+    # Multiplies the given number by 2 using bit-shifting.
+    # Left-shifting a binary number by one position (n << 1) moves all bits to the left which then adds a zero at the rightmost position
+    #This makes it double
 
+    
+    return n
