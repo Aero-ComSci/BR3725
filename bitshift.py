f78a2ba26301ab4582a74d3acd75f56fc44449f6
aasthananda <77813827+aasthananda@users.noreply.github.com>
2025-03-07 15:12:34 -0800

    Update bitshift.py

--- bitshift.py
+++ bitshift.py
@@ -1,5 +1,10 @@
-def multiply(n):
-    #Add the code and an explanation on how to 
-    #multiply a number by 2 using bitshift
-    return n
+def multiply(a,b): #function
+    result = 0 #final result that will be printed later
+    while b>0: #checks if b is greater than 0 which makes it positive
+        if b%2==1: #checks to see if remainder of b if divided by 2 is 1, hence b is odd. Once b is 0, the multiplication has ended. 
+            result += a #if previous condition met, adds a to result
+        a <<= 1 #shifts left (it multiplies by 2 each iteration)
+        b >>= 1 #shifts right (it divides b by 2 each iteration)
+    print(result)
+
 
