a362468e211561ade4749c646eaf91e773819c69
RohitDen <157422362+RohitDen@users.noreply.github.com>
2025-03-07 15:23:58 -0800

    Update bitshift.py

--- bitshift.py
+++ bitshift.py
@@ -1,3 +1,11 @@
-def multiplication(a,):
-    return n
+def multiply(a, b):
+    result = 0 
+    
+    while b > 0:  
+        if b & 1:  #This section checks if the least significant bit is 1, and if it is, it is used for the final production.
+            result += a  The value of a is added to b if the least significant bit is 1
+        
+        a <<= 1  #A left shift on b is applied here, which is equivelant to multiplying by 2
+        b >>= 1  #A right shift on b is applied here, which is equivelant to dividing b by 2
 
+    return result #after all the bits of b have been processed, result is produced as the final amount
